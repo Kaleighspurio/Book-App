612c260e580a83a3b4261af7d7122596c13078ba
Kaleigh Spurio <kaleighastell@gmail.com>
2020-09-24 14:48:01 -0400

    set up api call to google books

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,34 @@
 const router = require('express').Router();
+const axios = require('axios');
 const isAuthenticated = require('../../config/middleware/isAuthenticated');
 
 router.get('/secrets', isAuthenticated, (req, res) => {
   res.json('Talk is cheap. Show me the code. -Linus Torvalds');
+});
+
+// Get from Google Books by keyword
+router.get('/search/keyword/:keyword', (req, res) => {
+  console.log(req.params);
+  axios.get(`https://www.googleapis.com/books/v1/volumes?q=${req.params.keyword}&maxResults=40&key=${process.env.API_KEY}`).then((response) => {
+    res.json(response.data);
+  });
+});
+
+
+// Get from Google Books by keyword and author
+router.get('/search/keyword/:keyword/author/:author', (req, res) => {
+  console.log(req.params);
+  axios.get(`https://www.googleapis.com/books/v1/volumes?q=${req.params.keyword}+inauthor:${req.params.author}&maxResults=40&key=${process.env.API_KEY}`).then((response) => {
+    res.json(response.data);
+  });
+});
+
+// Get from google books by keyword adn subject
+router.get('/search/keyword/:keyword/subject/:subject', (req, res) => {
+  console.log(req.params);
+  axios.get(`https://www.googleapis.com/books/v1/volumes?q=${req.params.keyword}+suject:${req.params.subject}&maxResults=40&key=${process.env.API_KEY}`).then((response) => {
+    res.json(response.data);
+  });
 });
 
 module.exports = router;
